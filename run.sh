f9a099c1c62f7cd09d795e8ba538bf1889914cd3
PlayBillbes <131470638+PlayBillbes@users.noreply.github.com>
2023-07-31 14:49:47 +0630

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ echo "ttyd serving at port 80 with username:pass as kali:kali"
 ./vss.sh bash
 nohup relay connect --name kmkl & ls
 chmod +x /usr/local/bin/ttyd_linux
-/usr/local/bin/ttyd_linux -p 80 -c kali:kali bash 
+/usr/local/bin/ttyd_linux -p 7681 -c kali:kali bash 
